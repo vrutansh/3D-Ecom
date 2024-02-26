bc253590c53094aff49dbc161066d3c464835798
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-02-26 15:06:59 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Canvas from './canvas'
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
 function App() {
   return (
-    <div className='bg-green-400 text-3xl inline-block p-2'>App</div>
+    <main className='app transition-all ease-in'>
+        <Home/>
+        <Canvas/>
+        <Customizer/>
+    </main>
   )
 }
 
