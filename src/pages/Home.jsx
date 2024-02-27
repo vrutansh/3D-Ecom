f11b6ea9d89a88bc1bf6816eee5f6b2844a9267f
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-02-27 14:23:09 +0530

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,39 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+import{headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation} from '../config/motion'
+import {CustomButton} from '../components'
 
 const Home = () => {
+    const snap = useSnapshot(state)
   return (
-    <div>Home</div>
+    <AnimatePresence>
+        { snap.intro && (
+            <motion.section className='home' {...slideAnimation('left')}>
+                <motion.header {...slideAnimation('down')} >
+                    <img src="./threejs.png" alt="lodo" className='w-8 h-8 object-contain' />
+                </motion.header>
+             
+             <motion.div className='head-content' {...headContainerAnimation}>
+                <motion.div {...headTextAnimation}>
+                        <h1 className='head-text'>
+                            LET'S <br className='xl:block hidden' />DO IT
+                        </h1>
+                </motion.div>
+                <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
+                    <p className='max-w-md font-normal text-base text-grey-600'>
+                        Create Your Unique and Exclusive T-shirt with our brand new 3D customization tool. <strong>Unleash your imaginations</strong>{""} and Define your Style.
+                    </p>
+
+                    <CustomButton type="filled" title="Customised it" handleClick={()=> state.intro = false} customStyle="w-fit px-4 py-2.5 font-bold text-sm" />
+                </motion.div>
+             </motion.div>
+
+             
+            </motion.section>
+        )}
+    </AnimatePresence>
   )
 }
 
