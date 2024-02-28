95152730b0d29d9c4ffe0cc32cb6ea50eef733e0
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-02-28 11:45:36 +0530

    shirt and tabs ok

---
+++ src/canvas/Camerarig.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Camerarig = () => {
+  return (
+    <div>Camerarig</div>
+  )
+}
+
+export default Camerarig
--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react'; 
+import { Canvas } from '@react-three/fiber';
+import { Environment, Center } from '@react-three/drei';
 
-const Canvas = () => {
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import Camerarig from './Camerarig';
+
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+        <ambientLight intensity={0.5}/>
+        <Environment preset='city'/>
+
+        {/* <Camerarig>
+          <Backdrop/> */}
+          <Center>
+            <Shirt/>
+          </Center>
+        {/* </Camerarig> */}
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
